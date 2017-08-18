aa31268f23bfc39586dc0d1543d90bdc5bf969b7
Agus <agusalexander@hotmail.com>
2017-08-17 22:34:22 -0300

    added embedded h2 server

--- src/main/java/com/mycompany/app/persistencia/conexion/Conexion.java
+++ src/main/java/com/mycompany/app/persistencia/conexion/Conexion.java
@@ -1,32 +1,73 @@
 package com.mycompany.app.persistencia.conexion;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
+
+
 
 public class Conexion 
 {
 	public static Conexion instancia;
 	private Connection conexion;
-	
-	public Conexion()
-	{
-		try
-		{
 
-			conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda","root","root");
-			System.out.println("Conexion exitosa");
+
+
+
+	public Conexion()
+
+	{
+
+		try {
+			Class.forName("org.h2.Driver");
+			String url = "jdbc:h2:" + "~//agenda";
+			String user = "root";
+			String password = "root";
+			conexion = null;
+
+			conexion = DriverManager.getConnection(url, user, password);
+
+
 		}
 		catch(Exception e)
 		{
-			System.out.println("Conexion fallida");
+			System.out.println(e.toString() +"\n Conexion H2 fallida");
 		}
 	}
+
+
+	public static void initDB(){
+		String initDB="CREATE TABLE `personas` \n" +
+				"(\n" +
+				"  `idPersona` int(11) NOT NULL AUTO_INCREMENT,\n" +
+				"  `Nombre` varchar(45) NOT NULL,\n" +
+				"  `Telefono` varchar(20) NOT NULL,\n" +
+				"  PRIMARY KEY (`idPersona`)\n" +
+				");";
+
+
+		Conexion conexion=Conexion.getConexion();
+
+		Connection conn=conexion.getSQLConexion();
+
+		Statement statement;
+		try {
+			statement=conn.createStatement();
+			statement.execute(initDB);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+
+
+	}
+
 	
 	public static Conexion getConexion()   
 	{								
 		if(instancia == null)
 		{
+
 			instancia = new Conexion();
+			initDB();
 		}
 		return instancia;
 	}
