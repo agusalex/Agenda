7dad9c0f35f4241c0284a0774119abf060c62614
MaxPhoenix <lencinamaxiezequiel@gmail.com>
2017-08-30 01:57:21 -0300

    fix report. correct grouping

---
+++ sql/scriptInsert.sql
@@ -0,0 +1,40 @@
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(1, Max Lencina, 1234567890, Falsa, 123, 1, 1, max@gmail.com, 05/08/1995, gmail.com);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(2, Agus Alex, 1234567832, Gaspar Campos, 1232, 12, 12, agus@gmail.com, 05/08/1995, gmail.com);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(3, Rober Ger, 1234512890, Lujan, 3245, 1, 1, rober@hotmail.com, 05/08/1995, hotmail.com);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(4, Danilo Eldelson, 1234567821, Cordoba, 1235, 1, 1, dani@gmail.com, 05/08/1995, gmail.com);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(5, Misael Britos, 1234567843, Falsa, 321, 1, 1, misa@hotmail.com, 05/08/1995, hotmail.com);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(6, Max Lencina, 1234567890, Lujan, 1234, 1, 1, maxi@ungs.com, 05/08/1995, ungs.com);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(7, Max Power, 1234567143, Cabildo, 1230, 12, A3, max@ungs.org, 05/08/1995, ungs.org);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(8, Agustin Perez, 1234567827, Justo, 879, 10, B3, agus@hotmail.com, 05/08/1995, hotmail.com);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(9, Fede Lencina, 1234562365, Salvador, 4352, 1, 1, fede@gmail.com, 05/08/1995, gmail.com);
+
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(10, Juan Perez, 1234341234, Tribulato, 9866, 12, C4, juanperez@yahoo.com.ar, 05/08/1995, yahoo.com.ar);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(11, Rodrigo Suarez, 1234563245, General Lemos, 0800, 1, 1, rodri@hotmail.com, 05/08/1995, hotmail.com);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(12, Pablo Perez, 1234876529, Libertad, 0907, 1, 1, pablito@ungs.com, 05/08/1995, ungs.com);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(13, Francisco Fernandez, 1234564321, Flores, 4532, 1, 1, pancho@hotmail.com, 05/08/1995, hotmail.com);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(14, Franco Lopez, 1234567342, Rivadavia, 1238, 1, 1, franco@gmail.com, 05/08/1995, gmail.com);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(15, Abel Sanchez, 1234569264, Verde, 009900, 1, 1, abi@ungs.org, 05/08/1995, ungs.org);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(16, Sabrina Perez, 1234564321, Verdadera, 1233, 1, 1, sabrina@ungs.com, 05/08/1995, ungs.com);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(17, Daniela Martinez, 1234565487, Roja, 3241, 1, 1, daniela@hotmail.com, 05/08/1995, hotmail.com);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(18, Camila Sanchez, 1234098734, Calle, 9843, 1, 1, cami@gmail.com, 05/08/1995, gmail.com);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(19, Roberto Juarez, 2341567890, Libertador, 4329, 1, 1, roberto@ungs.com, 05/08/1995, ungs.com);
+
+INSERT INTO personas(idPersona, nombre, telefono,calle,altura,piso,departamento,email,fechaNacimiento,mailServer) VALUES(20, Yolanda Perez, 1212127890, Boca, 7653, 1, 1, yolanda@ungs.org, 05/08/1995, ungs.org);
